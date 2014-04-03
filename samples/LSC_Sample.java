0d3f4d394a2c9885f4e22ff90aced99e15891039
Kevin Lubick <kjlubick@ncsu.edu>
2014-04-03 19:57:00 -0400

    Better example
    Using enum and multiple switch cases to show TableSwitch and
    LookupSwitch

--- samples/LSC_Sample.java
+++ samples/LSC_Sample.java
@@ -1,7 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+
 
 public class LSC_Sample {
 	public static final String CONSTANT_VAL_STRING = "GoodBye";
 	private static final String CONSTANT_VAL_STRING2 = "GoodBye2";
+	
+	enum Planets {
+		EARTH,MARS,VENUS, JUPITER;
+	}
 	
     public boolean test1(String s) {
         return s.equals("Hello");
@@ -51,58 +58,24 @@ public class LSC_Sample {
 		}
     }
     
-    /*
-     * public static int test11(java.lang.String);
-    Code:
-       0: aload_0       
-       1: dup           
-       2: astore_1      
-       3: invokevirtual #49                 // Method java/lang/String.hashCode:()I
-       6: lookupswitch  { // 2
-
-              69609650: 32
-
-            1871644881: 44
-               default: 60
-          }
-      32: aload_1       
-      33: ldc           #24                 // String Hello
-      35: invokevirtual #26                 // Method java/lang/String.equals:(Ljava/lang/Object;)Z
-      38: ifne          56
-      41: goto          60
-      44: aload_1       
-      45: ldc           #8                  // String GoodBye
-      47: invokevirtual #26                 // Method java/lang/String.equals:(Ljava/lang/Object;)Z
-      50: ifne          58
-      53: goto          60
-      56: iconst_1      
-      57: ireturn       
-      58: iconst_2      
-      59: ireturn       
-      60: aload_0       
-      61: ldc           #55                 // String Foo
-      63: invokevirtual #26                 // Method java/lang/String.equals:(Ljava/lang/Object;)Z
-      66: ifeq          71
-      69: iconst_3      
-      70: ireturn       
-      71: getstatic     #57                 // Field java/lang/System.out:Ljava/io/PrintStream;
-      74: aload_0       
-      75: invokevirtual #63                 // Method java/io/PrintStream.println:(Ljava/lang/String;)V
-      78: iconst_4      
-      79: ireturn   
-     */
+   
     
-    public static int test11(String s) {
+    public static int test11(String s, String s2) {
     	//no tag
     	switch (s) {
-		case "Hello":
+		case "Switch1":
 			return 1;
-		case CONSTANT_VAL_STRING:
+		case "switch2":
 			return 2;
+		case "switch3":
+			//tag
+			if (s2.equalsIgnoreCase("Foo6")) {
+				return 5;
+			}
 		}
     	
     	//tag
-    	if (s.equals("Foo")) {
+    	if (s.equals("Foo7")) {
     		return 3;
     	}
     	System.out.println(s);
@@ -110,7 +83,53 @@ public class LSC_Sample {
     	
     }
     
-    public static void main(String[] args) {
-		System.out.println(test10(null));
-	}
+    public static int test12(int n, String s2) {
+    	//no tag
+    	switch (n) {
+		case 1:
+			return 1;
+		case 2:
+			return 2;
+		case 3:
+		case 7:
+			//tag
+			if (s2.equalsIgnoreCase("Foo6")) {
+				return 5;
+			}
+		}
+    	
+    	//tag
+    	if (s2.equals("Foo7")) {
+    		return 3;
+    	}
+    	System.out.println(s2);
+    	return 4;
+    	
+    }
+    
+    public static int test13(Planets p, String s2) {
+    	//no tag
+    	switch (p) {
+		case EARTH:
+			return 1;
+		case MARS:
+			return 2;
+		case JUPITER:
+			//tag
+			if (s2.equalsIgnoreCase("Foo6")) {
+				return 5;
+			}
+		default:
+			break;
+		}
+    	
+    	//tag
+    	if (s2.equals("Foo7")) {
+    		return 3;
+    	}
+    	System.out.println(s2);
+    	return 4;
+    	
+    }
+    
 }
